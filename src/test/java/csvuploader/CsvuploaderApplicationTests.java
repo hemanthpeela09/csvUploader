910f10e7728d5cc2f65c7a50c5410db9cbda5205
Hemanth Peela <hemanthpeela@zohomail.in>
2023-04-20 23:27:08 +0530

    init commit
    Project spring init update

---
+++ src/test/java/csvuploader/CsvuploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package csvuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CsvuploaderApplicationTests {
+	
+	@Test
+	void contextLoads() {
+		
+	}
+}
